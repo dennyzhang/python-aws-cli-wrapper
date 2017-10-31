702e5ed18c7307267da095e87e5d90380f893802
dennyzhang <denny@dennyzhang.com>
2017-10-31 17:09:56 -0500

    code change

--- route53/delete_aws.sh
+++ route53/delete_aws.sh
@@ -9,11 +9,29 @@
 ## Description :
 ## --
 ## Created : <2017-10-31>
-## Updated: Time-stamp: <2017-10-31 16:23:26>
+## Updated: Time-stamp: <2017-10-31 16:37:55>
 ##-------------------------------------------------------------------
-set -e
+# set -e
+function log() {
+    local msg=$*
+    date_timestamp=$(date +['%Y-%m-%d %H:%M:%S'])
+    echo -ne "$date_timestamp $msg\n"
+    
+    if [ -n "$LOG_FILE" ]; then
+        echo -ne "$date_timestamp $msg\n" >> "$LOG_FILE"
+    fi
+}
 
-cname="mytest.abc.com."
-dns_value="fake.test.ai"
-python route53_geneate_json.py --cname "$cname" --value "$dns_value"
+# ./delete_aws.sh "my.domain.test." "./delete1.txt" "ZEFB94UUWPTCB"
+dns_value=${1?}
+cname_file=${2?}
+host_zone_id=${3?}
+json_file="aws_dns.json"
+for cname in $(cat "$cname_file"); do
+    log "Generate json for $cname, and delete the route53 CNAME record"
+    python route53_geneate_json.py --cname "$cname" --value "$dns_value" --json_file "$json_file"
+    # cat "$json_file"
+    echo "aws route53 change-resource-record-sets --hosted-zone-id $host_zone_id --change-batch file://./$json_file"
+    aws route53 change-resource-record-sets --hosted-zone-id "$host_zone_id" --change-batch file://./$json_file
+done
 ## File: hello ends
--- route53/route53_geneate_json.py
+++ route53/route53_geneate_json.py
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2017-10-31>
-## Updated: Time-stamp: <2017-10-31 16:15:24>
+## Updated: Time-stamp: <2017-10-31 16:36:41>
 ##-------------------------------------------------------------------
 import argparse
 
@@ -20,14 +20,15 @@ def generate_new_file(json_tmpl_file, target_file, cname, value):
             if "\"Name\":" in row:
                 row = "                \"Name\": \"%s\",\n" % (cname)
             if "\"Value\":" in row:
-                row = "                    \"Value\": \"%s\",\n" % (value)
+                row = "                      \"Value\": \"%s\"\n" % (value)
             target_f.write(row)
         target_f.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cname', required=True, help="DNS CNAME to delete", type=str)
-    parser.add_argument('--value', required=True, help="DNS CNAME to delete", type=str)
+    parser.add_argument('--value', required=True, help="DNS value", type=str)
+    parser.add_argument('--json_file', required=True, help="json file", type=str)
     l = parser.parse_args()
-    generate_new_file("aws_dns_tmpl.json", "aws_dns.json", l.cname, l.value)
+    generate_new_file("aws_dns_tmpl.json", l.json_file, l.cname, l.value)
 ## File: route53_geneate_json.py ends
